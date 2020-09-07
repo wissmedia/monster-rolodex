7b895b678fc4037916677a68abd3e8185b85ab7d
wissmedia <44659148+wissmedia@users.noreply.github.com>
2020-09-07 15:52:18 +0800

    Updates

---
+++ precache-manifest.8cc205bc11788dbdb585bf88dfa0c8ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e1228aa2508554cad6d5cb4f173ed8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6005adad63996a036db6",
+    "url": "/monster-rolodex/static/css/main.31722687.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6005adad63996a036db6",
+    "url": "/monster-rolodex/static/js/main.32a568db.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
